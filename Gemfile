6682631c00e7c221678662235926f39a9d23c42d
Stan Smith <adtech@alaska.net>
2015-03-03 17:25:23 -0900

    - clean up on error message handling - return error message block as plain text when when 'auto' requested - changed method of removing full path from validation messages - set gemfile to ignore unicorn in windows environment

--- Gemfile
+++ Gemfile
@@ -1,17 +1,24 @@
 source 'https://rubygems.org'
 ruby '2.1.5'
+
 #use unicorn server
-gem 'unicorn'
+platforms :ruby do # linux
+    gem 'unicorn'
+end
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
+
 # Use sqlite3 as the database for Active Record
 group :development do
   gem 'sqlite3'
 end
+
 #for heroku
 group :production do
   gem 'pg'
 end
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/api/v1/translators_controller.rb
+++ app/controllers/api/v1/translators_controller.rb
@@ -18,7 +18,9 @@
 #   ... mdTranslator returns error messages
 #   Stan Smith 2014-11-10 remove absolute path information from error messages
 #   Josh Bradley 2014-11-12 fix message output for plain response
-#   Stan Smith 2015-01-16 changed ADIWG::Mdtranslate.translate() to keyword parameter list
+#   Stan Smith 2015-01-16 changed ADIWG::Mdtranslator.translate() to keyword parameter list
+#   Stan Smith 2015-03-03 clean up of error message handling
+#   Stan Smith 2015-03-03 return error message in plain text when 'auto' selected
 
 class Api::V1::TranslatorsController < ApplicationController
 
@@ -60,71 +62,68 @@ class Api::V1::TranslatorsController < ApplicationController
 		# load any output returned from mdTranslator into the response hash
 		@responseInfo[:data] = @mdReturn[:writerOutput]
 
-		# check for validation errors and prepare error messages if any
-		if @mdReturn[:readerStructurePass] && @mdReturn[:readerValidationPass]
+		# check for errors returned by parser, validator, reader, and writer
+		if @mdReturn[:readerStructurePass] && @mdReturn[:readerValidationPass] && @mdReturn[:readerExecutionPass] && @mdReturn[:writerPass]
 			# no validation errors were detected
 			@responseInfo[:success] = true
 		else
-			# errors messages were returned by mdTranslator's validation of input
-			# therefore ...
-			# ... no input was passed to the mdTranslator writer and
-			# ... no writer output was returned by the mdTranslator
+			# errors messages were returned by mdTranslator's parser, validator, reader, or writer
 			@responseInfo[:success] = false
 
 			# pass all information received from the mdTranslator to the requester
 			# ... to assist in error resolution
-			# do not alter the @mdReturn object as returned from mdTranslator
-			# rewrite message to
-			# ... shorten structure message to 1000 characters
 			# ... remove absolute paths from validation messages
 			@responseInfo[:messages] = @mdReturn
 
-			# handle error messages individually because some contain '[]' or '{}'
-			# ...which confuses array methods
-			unless @mdReturn[:readerStructurePass].nil?
-				aMessages = []
-				if @mdReturn[:readerStructurePass]
-					aMessages << "Success - Input structure is valid\n"
-				else
-					aMessages << "Fail - Structure of input file is invalid - see following message(s):\n"
-					@mdReturn[:readerStructureMessages].each do |message|
-						# the mdTranslator parser returns the entire input file if it finds a structure error
-						# ... there is no indication in the file where the error(s) occurred,
-						# ... therefore the returned file is truncated to 1000 characters
-						s = message.to_s.slice!(0,1000)
-						if s.length == 1000
-							s += ' ...'
-						end
-						aMessages << s
-					end
-				end
-				@responseInfo[:messages][:readerStructureMessages] = aMessages
-			end
+            # handling of parser errors
+            if @mdReturn[:readerStructurePass]
+                @responseInfo[:messages][:readerStructureMessages].insert(0, "Success - Input structure is valid\n")
+            else
+                @responseInfo[:messages][:readerStructureMessages].insert(0,"Fail - Structure of input file is invalid - see following message(s):\n")
+            end
 
-			unless @mdReturn[:readerValidationPass].nil?
-				aMessages = []
-				if @mdReturn[:readerValidationPass]
-					aMessages << "Success - Input content passes schema definition\n"
-				else
-					aMessages << "Fail - Input content did not pass schema validation - see following message(s):\n"
+            # handling validator messages
+            if @mdReturn[:readerValidationPass].nil?
+                @responseInfo[:messages][:readerValidationMessages].insert(0, "Validator was not called\n")
+            elsif @mdReturn[:readerValidationPass]
+                @responseInfo[:messages][:readerValidationMessages].insert(0, "Success - Input content passes schema definition\n")
+            else
+                # the json schema validator returns full expanded paths to gem
+                # these full paths may pose a security risk and are removed from the messages
+                # find gem path to remove from messages
+                gem_root = ADIWG::MdjsonSchemas::Utils.schema_dir.match(/(^.+)\/lib/i).captures[0]
+                gem_path = File.join(gem_root, 'schema')
+                gem_path[0] = gem_path[0].downcase
 
-					# the json schema validator returns full expanded paths to gem
-					# these full paths may pose a security risk and are removed from the messages
-					# find gem path to remove from messages
-					spec = Gem::Specification.find_by_name('adiwg-json_schemas')
-					gem_root = spec.gem_dir
-					gem_path = File.join(gem_root, 'schema')
-					gem_path[0] = gem_path[0].downcase
+                # replace gem_path in messages with '...'
+                aMessages = []
+                @mdReturn[:readerValidationMessages].each do |hMessage|
+                    sMessage = hMessage.to_s
+                    sMessage = sMessage.gsub(gem_path, '...')
+                    aMessages << sMessage
+                end
+                @responseInfo[:messages][:readerValidationMessages] = aMessages
 
-					# replace gem_path in messages with '...'
-					@mdReturn[:readerValidationMessages].each do |hMessage|
-						sMessage = hMessage.to_s
-						sMessage = sMessage.gsub(gem_path, '...')
-						aMessages << sMessage
-					end
-				end
-				@responseInfo[:messages][:readerValidationMessages] = aMessages
-			end
+                @responseInfo[:messages][:readerValidationMessages].insert(0, "Fail - Input content did not pass schema validation - see following message(s):\n")
+            end
+
+            # handling reader messages
+            if @mdReturn[:readerExecutionPass].nil?
+                @responseInfo[:messages][:readerExecutionMessages].insert(0, "Reader was not called\n")
+            elsif @mdReturn[:readerExecutionPass]
+                @responseInfo[:messages][:readerExecutionMessages].insert(0, "Success - Input file was read\n")
+            else
+                @responseInfo[:messages][:readerExecutionMessages].insert(0,"Fail - Reading of input file did not complete  - see following message(s):\n")
+            end
+
+            # handling of writer messages
+            if @mdReturn[:writerPass].nil?
+                @responseInfo[:messages][:writerMessages].insert(0, "Writer was not called\n")
+            elsif @mdReturn[:writerPass]
+                @responseInfo[:messages][:writerMessages].insert(0, "Success - Metadata file was created\n")
+            else
+                @responseInfo[:messages][:writerMessages].insert(0,"Fail - Creation of metadata file failed to complete  - see following message(s):\n")
+            end
 
 		end
 
@@ -167,37 +166,23 @@ class Api::V1::TranslatorsController < ApplicationController
 							end
 
 					end
-				else
-					# validation errors were found and will be returned to requester
-					# validation errors are caught before calling the writer
-					# so writerFormat will generally be nil
-					case @mdReturn[:writerFormat]
-						when nil
-							request.format = 'plain'
-							s = ''
-							messages = ActiveSupport::HashWithIndifferentAccess.new(@responseInfo[:messages])
-							messages.each do |key, message|
-								s += (key + ': ' + message.to_s + "\n")
-							end
-							if params[:callback] == ''
-								render plain: s
-							else
-								render json: s, callback: params[:callback]
-							end
+                else
+					# errors were found and will be returned to requester
+					# return plain text
+                    request.format = 'plain'
+                    s = ''
+                    messages = ActiveSupport::HashWithIndifferentAccess.new(@responseInfo[:messages])
+                    messages.each do |key, message|
+                        s += (key + ': ' + message.to_s + "\n")
+                    end
+                    if params[:callback] == ''
+                        render plain: s
+                    else
+                        render json: s, callback: params[:callback]
+                    end
 
-						when 'xml'
-							# if errors were detected by an XML writer
-							# the iso19115_2 XML writer does not error check at this time
-							# this is an unused path
-							# render 'create.xml.builder'
+                end
 
-						when 'json'
-							# if errors were detected by a JSON writer
-							# no JSON writer are available at this time
-							# this is an unused path
-							# render json: @responseInfo[:data]
-					end
-				end
 
 			when 'plain'
 				# text/plain was requested
@@ -213,7 +198,7 @@ class Api::V1::TranslatorsController < ApplicationController
 					end
 
 				else
-					# validation errors were found and will be returned to requester
+					# errors were found and will be returned to requester
 					s = ''
 					messages = ActiveSupport::HashWithIndifferentAccess.new(@responseInfo[:messages])
 					messages.each do |key, message|
