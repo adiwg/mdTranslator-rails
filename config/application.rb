cea0a1c44c1b7d70f057ed2a473d34b5f6a2e0d1
Tim Zwolak <timothypage@gmail.com>
2022-05-17 15:03:07 -0600

    add some security related headers

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,12 @@ module MdtranslatorRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'SAMEORIGIN',
+      'X-XSS-Protection' => '1; mode=block',
+      'X-Content-Type-Options' => 'nosniff'
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
