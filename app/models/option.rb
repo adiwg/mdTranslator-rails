24506384f7da52dac6de17f8ea9f82e1b3212262
Johnathan Aspinwall <jwaspin@gmail.com>
2023-10-17 13:41:28 -0600

    Rename 19115-1 to 19115-3
    Also 19115_1 to 19115_3

--- app/models/option.rb
+++ app/models/option.rb
@@ -12,7 +12,7 @@ class Option
    def self.getOptionList
       {
          reader: %w[mdJson sbJson fgdc],
-         writer: %w[iso19115_2 iso19110 html mdJson sbJson fgdc simple_html],
+         writer: %w[iso19115_3 iso19115_2 iso19110 html simple_html mdJson sbJson fgdc],
          format: %w[auto plain json xml],
          validate: %w[none normal strict],
          showAllTags: %w[true false],
